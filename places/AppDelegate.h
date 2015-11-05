5161fc5712e589f0b40899d8070169f1b16c56ae
Adrian Rodriguez <adrianr0120@gmail.com>
2015-11-05 18:20:26 -0500

    Initial Commit

---
+++ places/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  places
+//
+//  Created by Adrian Rodriguez on 11/5/15.
+//  Copyright © 2015 totemc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ places/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  places
+//
+//  Created by Adrian Rodriguez on 11/5/15.
+//  Copyright © 2015 totemc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
